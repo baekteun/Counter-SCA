4d7b09566fb6c96b1671ab7ef5635236ea27929a
baegteun <baegteun@gmail.com>
2022-01-23 23:14:41 +0900

    :heavy_minus_sign: :: CombineMoya

--- ThirdPartyLib/Project.swift
+++ ThirdPartyLib/Project.swift
@@ -4,12 +4,10 @@ import ProjectDescriptionHelpers
 let project = Project.dynamicFramework(
     name: "ThirdPartyLib",
     packages: [
-        .Moya,
         .ComposableArchitecture
     ],
     deploymentTarget: .iOS(targetVersion: "14.0", devices: [.iphone, .ipad]),
     dependencies: [
-        .SPM.CombineMoya,
         .SPM.ComposableArchitecture,
     ]
 )
