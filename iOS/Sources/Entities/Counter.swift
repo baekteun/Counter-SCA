a7676abe2e51911bce2bbf3e590af8437716bb02
baegteun <baegteun@gmail.com>
2022-01-23 23:38:40 +0900

    :sparkles: :: CounterState

---
+++ iOS/Sources/Entities/Counter.swift
@@ -0,0 +1,17 @@
+//
+//  Counter.swift
+//  Counter
+//
+//  Created by 최형우 on 2022/01/23.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import Foundation
+
+struct CounterState: Equatable, Identifiable{
+    var count = 0
+    var secret = Int.random(in: -100...100)
+    
+    var id: UUID = UUID()
+}
+
